5bc8c7bc0e1d1865a3a9e5b7c0ec246f1fda0a97
Bhumil Modi <bhumilmodi2002@gmail.com>
2021-12-13 10:26:58 +0530

    Java Practice Codes

--- minchangeseq.java
+++ minchangeseq.java
@@ -6,7 +6,6 @@ public class minchangeseq {
     }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         for (int i = 0; i <= Math.pow(2, n) - 1; i++) {
